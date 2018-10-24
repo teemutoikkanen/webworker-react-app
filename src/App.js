c39a3919bbd71a408336fab8ab99ac153378ac4c
Toikkanen Teemu <teemu.toikkanen@aalto.fi>
2018-10-24 17:37:14 +0300

    slide-margin vs transform test

--- src/App.js
+++ src/App.js
@@ -1,45 +1,31 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 class App extends Component {
-
-
   someSlowComputation() {
-
     var i;
     var j = 0;
-    for (i = 0; i < 10000000000; i++) {
-      j = j + i**2
+    var n = 10000000000;
+    for (i = 0; i < n; i++) {
+      j = j + i ** 2;
     }
-
-    return j
-
-
   }
-  alertFunc() {
-    window.alert("derp");
 
+  alertFunc() {
+    window.alert("alert");
   }
 
   render() {
     return (
       <div className="App">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            React-logo rotating /w CSS.
-          </p>
+        <img src={logo} className="App-logo" alt="logo" />
+        React-logo rotating with CSS transform: rotate().
+        <img src={logo} className="App-logo2" alt="logo" />
+        React-logo sliding with CSS margin-left().
 
-
-        <button onClick={this.someSlowComputation}>
-          someHeavyComputation
-        </button>
-
-        <button onClick={this.alertFunc}>
-          alertButton
-        </button>
-
-
+        <button onClick={this.someSlowComputation}>someSlowComputation</button>
+        <button onClick={this.alertFunc}>alertButton</button>
       </div>
     );
   }
